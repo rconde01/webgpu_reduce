b5144b8dc3455859b01a939947b85ba9c83c69ef
Rob Conde <Rob.Conde@ai-solutions.com>
2023-09-24 14:13:13 -0400

    fix typescript naming conventions

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import $, { data } from "jquery";
 
 import shader from "./wgsl/reduce.wgsl";
 
-function reduce_cpu(input: Float32Array): number {
+function reduceCpu(input: Float32Array): number {
   // TODO: Use Kahan summation?
   var result = 0;
 
@@ -13,81 +13,81 @@ function reduce_cpu(input: Float32Array): number {
   return result;
 }
 
-function get_workgroup_size_options(
+function getWorkgroupSizeOptions(
   vendor: string,
-  max_workgroup_size: number,
-  max_workgroups: number,
-  max_workgroup_storage_size: number,
-  bytes_per_workgroup_element: number,
-  num_input_points: number
+  maxWorkgroupSize: number,
+  maxWorkgroups: number,
+  maxWorkgroupStorageSize: number,
+  bytesPerWorkgroupElement: number,
+  numInputPoints: number
 ): number[] {
-  var workgroup_size_options: number[] = [];
+  var workgroupSizeOptions: number[] = [];
 
-  var subgroup_size = 32;
+  var subgroupSize = 32;
 
-  const vendor_lower_case = vendor.toLowerCase();
+  const vendorLowerCase = vendor.toLowerCase();
 
   // This is just a guess based on typical cases - these could be wrong in general
   // or drivers might dynamically change them based on a variety of factors.
-  if (vendor_lower_case.includes("nvidia")) {
-    subgroup_size = 32;
-  } else if (vendor_lower_case.includes("intel")) {
-    subgroup_size = 32;
-  } else if (vendor_lower_case.includes("apple")) {
-    subgroup_size = 32;
+  if (vendorLowerCase.includes("nvidia")) {
+    subgroupSize = 32;
+  } else if (vendorLowerCase.includes("intel")) {
+    subgroupSize = 32;
+  } else if (vendorLowerCase.includes("apple")) {
+    subgroupSize = 32;
   } else if (
-    vendor_lower_case.includes("amd") ||
-    vendor_lower_case.includes("ati")
+    vendorLowerCase.includes("amd") ||
+    vendorLowerCase.includes("ati")
   ) {
-    subgroup_size = 64;
+    subgroupSize = 64;
   }
 
-  const max_invocations_per_dispatch = max_workgroup_size * max_workgroups;
+  const maxInvocationsPerDispatch = maxWorkgroupSize * maxWorkgroups;
 
   // We assume the entire input array can be reduced once in a single
   // dispatch.
-  if (num_input_points > max_invocations_per_dispatch) {
-    return workgroup_size_options;
+  if (numInputPoints > maxInvocationsPerDispatch) {
+    return workgroupSizeOptions;
   }
 
-  var workgroup_size = max_workgroup_size;
+  var workgroupSize = maxWorkgroupSize;
 
-  while (workgroup_size >= subgroup_size) {
-    let first_num_groups = Math.ceil(num_input_points / workgroup_size);
+  while (workgroupSize >= subgroupSize) {
+    let firstNumGroups = Math.ceil(numInputPoints / workgroupSize);
 
     if (
-      first_num_groups <= max_workgroups &&
-      workgroup_size * bytes_per_workgroup_element <= max_workgroup_storage_size
+      firstNumGroups <= maxWorkgroups &&
+      workgroupSize * bytesPerWorkgroupElement <= maxWorkgroupStorageSize
     ) {
-      workgroup_size_options.push(workgroup_size);
+      workgroupSizeOptions.push(workgroupSize);
     }
 
-    workgroup_size /= 2;
+    workgroupSize /= 2;
   }
 
-  return workgroup_size_options;
+  return workgroupSizeOptions;
 }
 
 interface TestCase {
   algorithm: string;
-  num_points: number;
-  workgroup_size: number;
+  numPoints: number;
+  workgroupSize: number;
 }
 
 let testCases: TestCase[] = [];
 let adapter: GPUAdapter | null;
 let device: GPUDevice | null;
 let vendor: string | undefined;
-let max_workgroup_size: number;
-let max_workgroups: number;
-let max_workgroup_storage_size: number;
-let working_buffer_0: GPUBuffer;
-let working_buffer_1: GPUBuffer;
-let test_data_buffer: GPUBuffer;
-let reduce_shader: GPUShaderModule;
-let bind_group_layout: GPUBindGroupLayout;
-let compute_pipeline_layout: GPUPipelineLayout;
-let compute_pipeline: GPUComputePipeline;
+let maxWorkgroupSize: number;
+let maxWorkgroups: number;
+let maxWorkgroupStorageSize: number;
+let workingBuffer0: GPUBuffer;
+let workingBuffer1: GPUBuffer;
+let testDataBuffer: GPUBuffer;
+let reduceShader: GPUShaderModule;
+let bindGroupLayout: GPUBindGroupLayout;
+let computePipelineLayout: GPUPipelineLayout;
+let computePipeline: GPUComputePipeline;
 
 let init = async () => {
   adapter = await navigator.gpu?.requestAdapter();
@@ -96,35 +96,34 @@ let init = async () => {
       maxComputeWorkgroupSizeX: adapter.limits.maxComputeWorkgroupSizeX,
       maxComputeInvocationsPerWorkgroup:
         adapter.limits.maxComputeInvocationsPerWorkgroup,
-        maxStorageBufferBindingSize: adapter.limits.maxStorageBufferBindingSize
+      maxStorageBufferBindingSize: adapter.limits.maxStorageBufferBindingSize,
     },
   })) as GPUDevice;
   vendor = (await adapter?.requestAdapterInfo())?.vendor;
 
-  max_workgroup_size = device.limits.maxComputeInvocationsPerWorkgroup;
-  max_workgroups = device.limits.maxComputeWorkgroupsPerDimension;
-  max_workgroup_storage_size = device.limits.maxComputeWorkgroupStorageSize;
+  maxWorkgroupSize = device.limits.maxComputeInvocationsPerWorkgroup;
+  maxWorkgroups = device.limits.maxComputeWorkgroupsPerDimension;
+  maxWorkgroupStorageSize = device.limits.maxComputeWorkgroupStorageSize;
 
-  working_buffer_0 = device.createBuffer({
+  workingBuffer0 = device.createBuffer({
     label: "working_buffer_0",
-    size: max_workgroups * max_workgroup_size * Float32Array.BYTES_PER_ELEMENT,
+    size: maxWorkgroups * maxWorkgroupSize * Float32Array.BYTES_PER_ELEMENT,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
   });
 
-  working_buffer_1 = device.createBuffer({
+  workingBuffer1 = device.createBuffer({
     label: "working_buffer_1",
     size:
-      (max_workgroups * max_workgroup_size * Float32Array.BYTES_PER_ELEMENT) /
-      2,
+      (maxWorkgroups * maxWorkgroupSize * Float32Array.BYTES_PER_ELEMENT) / 2,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
   });
 
-  reduce_shader = device.createShaderModule({
+  reduceShader = device.createShaderModule({
     label: "reduce_shader",
     code: shader,
   });
 
-  bind_group_layout = device.createBindGroupLayout({
+  bindGroupLayout = device.createBindGroupLayout({
     entries: [
       {
         // global input
@@ -145,31 +144,29 @@ let init = async () => {
     ],
   });
 
-  compute_pipeline_layout = device.createPipelineLayout({
-    bindGroupLayouts: [bind_group_layout],
+  computePipelineLayout = device.createPipelineLayout({
+    bindGroupLayouts: [bindGroupLayout],
     label: "compute_pipeline_layout",
   });
 
-  let num_points_options: number[] = [
-    1_024, 16_384, 131_072, 524_288, 4_194_304,
-  ];
+  let numPointsOptions: number[] = [1_024, 16_384, 131_072, 524_288, 4_194_304];
 
-  for (let num_points of num_points_options) {
-    let workgroup_size_options = get_workgroup_size_options(
+  for (let numPoints of numPointsOptions) {
+    let workgroupSizeOptions = getWorkgroupSizeOptions(
       vendor!,
-      max_workgroup_size,
-      max_workgroups,
-      max_workgroup_storage_size,
+      maxWorkgroupSize,
+      maxWorkgroups,
+      maxWorkgroupStorageSize,
       Float32Array.BYTES_PER_ELEMENT,
-      num_points
+      numPoints
     );
 
-    for (let workgroup_size of workgroup_size_options) {
-      for (let algo_number of [0, 1, 2, 3]) {
+    for (let workgroupSize of workgroupSizeOptions) {
+      for (let algoNumber of [0, 1, 2, 3]) {
         testCases.push({
-          algorithm: "reduce_" + algo_number.toString(),
-          num_points,
-          workgroup_size,
+          algorithm: "reduce_" + algoNumber.toString(),
+          numPoints: numPoints,
+          workgroupSize: workgroupSize,
         });
       }
     }
@@ -179,50 +176,52 @@ let init = async () => {
   window.postMessage("", "*");
 };
 
-const init_case = (device: GPUDevice, test_case: TestCase) => {
-  const test_data = new Float32Array(test_case.num_points);
+const initTestCase = (device: GPUDevice, testCase: TestCase) => {
+  const testData = new Float32Array(testCase.numPoints);
 
-  for (var i = 0; i < test_case.num_points; ++i) {
-    test_data[i] = Math.random();
+  for (var i = 0; i < testCase.numPoints; ++i) {
+    testData[i] = Math.random();
   }
 
-  test_data_buffer = device.createBuffer({
-    label: "test_data_buffer",
-    size: test_case.num_points * Float32Array.BYTES_PER_ELEMENT,
+  if (testDataBuffer !== undefined) {
+    testDataBuffer.destroy();
+  }
+
+  testDataBuffer = device.createBuffer({
+    label: "testDataBuffer",
+    size: testCase.numPoints * Float32Array.BYTES_PER_ELEMENT,
     usage: GPUBufferUsage.STORAGE,
     mappedAtCreation: true,
   });
 
   // Upload test data
   {
-    let mapped_test_buffer = new Float32Array(
-      test_data_buffer.getMappedRange()
-    );
-    mapped_test_buffer.set(test_data);
-    test_data_buffer.unmap();
+    let mappedTestBuffer = new Float32Array(testDataBuffer.getMappedRange());
+    mappedTestBuffer.set(testData);
+    testDataBuffer.unmap();
   }
 
-  compute_pipeline = device.createComputePipeline({
-    label: "compute_pipeline",
-    layout: compute_pipeline_layout,
+  computePipeline = device.createComputePipeline({
+    label: "computePipeline",
+    layout: computePipelineLayout,
     compute: {
-      module: reduce_shader,
-      entryPoint: test_case.algorithm,
+      module: reduceShader,
+      entryPoint: testCase.algorithm,
       constants: {
-        workgroup_size: test_case.workgroup_size,
+        workgroup_size: testCase.workgroupSize,
       },
     },
   });
 };
 
 function calculateNumDispatches(testCase: TestCase): number {
-  let dataSize = testCase.num_points;
+  let dataSize = testCase.numPoints;
 
   var numDispatches = 0;
 
-  while(dataSize > 1){
-    let num_workgroups = Math.ceil(dataSize / testCase.workgroup_size);
-    dataSize = num_workgroups;
+  while (dataSize > 1) {
+    let numWorkgroups = Math.ceil(dataSize / testCase.workgroupSize);
+    dataSize = numWorkgroups;
 
     numDispatches++;
   }
@@ -232,122 +231,122 @@ function calculateNumDispatches(testCase: TestCase): number {
 
 const reduce = (
   device: GPUDevice,
-  test_data_size: number,
-  workgroup_size: number,
-  test_correctness: boolean,
-  test_data_cpu: Float32Array | null
+  testDataSize: number,
+  workgroupSize: number,
+  testCorrectness: boolean,
+  testDataCpu: Float32Array | null
 ) => {
-  let command_encoder = device.createCommandEncoder({
-    label: "command_encoder",
+  let commandEncoder = device.createCommandEncoder({
+    label: "commandEncoder",
   });
 
-  let compute_pass = command_encoder.beginComputePass({
-    label: "compute_pass",
+  let computePass = commandEncoder.beginComputePass({
+    label: "computePass",
   });
 
-  compute_pass.setPipeline(compute_pipeline);
+  computePass.setPipeline(computePipeline);
 
   // Note: original CUDA does a copy between dispatches instead of swapping
   // buffers...in WebGPU you can't do that, you need multiple passes. I'm assuming
   // swapping buffers is faster but i don't know for sure.
 
-  const get_input_buffer = (pass_num: number): GPUBuffer => {
-    if (pass_num == 0) {
-      return test_data_buffer;
+  const getInputBuffer = (dispatchNum: number): GPUBuffer => {
+    if (dispatchNum == 0) {
+      return testDataBuffer;
     } else {
-      if (pass_num % 2 == 1) {
-        return working_buffer_0;
+      if (dispatchNum % 2 == 1) {
+        return workingBuffer0;
       } else {
-        return working_buffer_1;
+        return workingBuffer1;
       }
     }
   };
 
-  const get_output_buffer = (pass_num: number): GPUBuffer => {
-    if (pass_num % 2 == 0) {
-      return working_buffer_0;
+  const getOutputBuffer = (dispatchNum: number): GPUBuffer => {
+    if (dispatchNum % 2 == 0) {
+      return workingBuffer0;
     } else {
-      return working_buffer_1;
+      return workingBuffer1;
     }
   };
 
   // Keep reducing until data_size is 1
-  let data_size = test_data_size;
-  let dispatch_num = 0;
-  while (data_size > 1) {
-    const input_buffer = get_input_buffer(dispatch_num);
-    const output_buffer = get_output_buffer(dispatch_num);
+  let dataSize = testDataSize;
+  let dispatchNum = 0;
+  while (dataSize > 1) {
+    const inputBuffer = getInputBuffer(dispatchNum);
+    const outputBuffer = getOutputBuffer(dispatchNum);
 
-    compute_pass.setBindGroup(
+    computePass.setBindGroup(
       0,
       device.createBindGroup({
-        label: "bind_group_0",
-        layout: bind_group_layout,
+        label: "bindGroup0",
+        layout: bindGroupLayout,
         entries: [
           {
             binding: 0,
             resource: {
-              label: "global_input",
-              buffer: input_buffer,
-              size: data_size * Float32Array.BYTES_PER_ELEMENT,
+              label: "globalInput",
+              buffer: inputBuffer,
+              size: dataSize * Float32Array.BYTES_PER_ELEMENT,
             },
           },
           {
             binding: 1,
             resource: {
-              label: "global_output",
-              buffer: output_buffer,
+              label: "globalOutput",
+              buffer: outputBuffer,
             },
           },
         ],
       })
     );
 
-    let num_workgroups = Math.ceil(data_size / workgroup_size);
+    let numWorkgroups = Math.ceil(dataSize / workgroupSize);
 
-    compute_pass.dispatchWorkgroups(num_workgroups);
+    computePass.dispatchWorkgroups(numWorkgroups);
 
-    dispatch_num++;
-    data_size = num_workgroups;
+    dispatchNum++;
+    dataSize = numWorkgroups;
   }
 
-  compute_pass.end();
+  computePass.end();
 
-  device.queue.submit([command_encoder.finish()]);
+  device.queue.submit([commandEncoder.finish()]);
 
-  if (test_correctness) {
-    let transfer_command_encoder = device.createCommandEncoder({
+  if (testCorrectness) {
+    let transferCommandEncoder = device.createCommandEncoder({
       label: "transfer_command_encoder",
     });
 
-    const transfer_buffer = device.createBuffer({
-      label: "transfer_buffer",
+    const transferBuffer = device.createBuffer({
+      label: "transferBuffer",
       size: Float32Array.BYTES_PER_ELEMENT,
       usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
     });
 
-    transfer_command_encoder.copyBufferToBuffer(
-      get_output_buffer(dispatch_num - 1),
+    transferCommandEncoder.copyBufferToBuffer(
+      getOutputBuffer(dispatchNum - 1),
       0,
-      transfer_buffer,
+      transferBuffer,
       0,
       Float32Array.BYTES_PER_ELEMENT
     );
 
-    device.queue.submit([transfer_command_encoder.finish()]);
+    device.queue.submit([transferCommandEncoder.finish()]);
 
-    transfer_buffer.mapAsync(GPUMapMode.READ).then(() => {
-      console.log("CPU Reduce: " + reduce_cpu(test_data_cpu!));
+    transferBuffer.mapAsync(GPUMapMode.READ).then(() => {
+      console.log("CPU Reduce: " + reduceCpu(testDataCpu!));
 
-      var mapped_buffer = transfer_buffer.getMappedRange(
+      var mappedBuffer = transferBuffer.getMappedRange(
         0,
         Float32Array.BYTES_PER_ELEMENT
       );
 
-      console.log("GPU Reduce: " + new Float32Array(mapped_buffer)[0]);
+      console.log("GPU Reduce: " + new Float32Array(mappedBuffer)[0]);
 
-      transfer_buffer.unmap();
-      transfer_buffer.destroy();
+      transferBuffer.unmap();
+      transferBuffer.destroy();
     });
   }
 };
@@ -364,44 +363,46 @@ let caseStartTime: number;
 window.onmessage = () => {
   postMessagePending--;
 
-  const test_case = testCases[testIndex];
-
-  if (caseCount === 0) {
-    init_case(device!, test_case);
-    caseStartTime = Date.now();
-  }
-
-  for (let i = 0; i < casesPerPost; ++i) {
-    reduce(
-      device!,
-      test_case.num_points,
-      test_case.workgroup_size,
-      false,
-      null
-    );
-  }
-
-  caseCount += casesPerPost;
-
-  if (caseCount >= maxCaseCount) {
-    caseCount = 0;
-    const casesPerSecond = Math.floor(
-      (1000.0 * maxCaseCount) / (Date.now() - caseStartTime)
-    );
-    const numDispatches = calculateNumDispatches(testCases[testIndex]);
-    console.log(
-      `${test_case.algorithm} ${test_case.workgroup_size} ${test_case.num_points.toLocaleString()} ${numDispatches}: ${casesPerSecond.toLocaleString()}`
-    );
-
-    testIndex++;
-  }
-
-  if (testIndex === testCases.length) {
-    console.log("Tests complete.");
-    return;
-  }
-
   if (postMessagePending == 0) {
+    const testCase = testCases[testIndex];
+
+    if (caseCount === 0) {
+      initTestCase(device!, testCase);
+      caseStartTime = Date.now();
+    }
+
+    for (let i = 0; i < casesPerPost; ++i) {
+      reduce(
+        device!,
+        testCase.numPoints,
+        testCase.workgroupSize,
+        false,
+        null
+      );
+    }
+
+    caseCount += casesPerPost;
+
+    if (caseCount >= maxCaseCount) {
+      caseCount = 0;
+      const casesPerSecond = Math.floor(
+        (1000.0 * maxCaseCount) / (Date.now() - caseStartTime)
+      );
+      const numDispatches = calculateNumDispatches(testCases[testIndex]);
+      console.log(
+        `${testCase.algorithm} ${
+          testCase.workgroupSize
+        } ${testCase.numPoints.toLocaleString()} ${numDispatches}: ${casesPerSecond.toLocaleString()}`
+      );
+
+      testIndex++;
+    }
+
+    if (testIndex === testCases.length) {
+      console.log("Tests complete.");
+      return;
+    }
+
     postMessagePending++;
     window.postMessage("", "*");
   }
